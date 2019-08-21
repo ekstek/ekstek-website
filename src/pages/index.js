36ac5d2b98f0a7c5a3347be98da30681e536568d
KumailP <kumailpirzada@gmail.com>
2019-08-21 23:51:49 +0500

    Fixed bootstrap issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
-import '../styles/index.css'
-import NavBar from '../components/navbar'
-import Button from 'react-bootstrap/Button';
+import "../styles/index.css"
+import NavBar from "../components/navbar"
+import Button from "react-bootstrap/Button"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -9,12 +9,15 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <div> 
-     <NavBar />
-     <div className = "main-heading">
-       <h1>Lorem ipsum dolor sit amet</h1>
-       <h2>consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</h2>
-     </div>
+  <div>
+    <SEO />
+    <NavBar />
+    <div className="main-heading">
+      <h1>Lorem ipsum dolor sit amet</h1>
+      <h2>
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+      </h2>
+    </div>
   </div>
 )
 
