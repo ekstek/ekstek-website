410e466233029fa589f60fb9856d62a9fc9c7b02
Shayan Mustafa <mustafa.shayan9@gmail.com>
2019-09-08 21:51:28 +0500

    favicon config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/images/favicon96x96.png`
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
